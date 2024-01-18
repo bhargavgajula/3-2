2a361fd96ffb0ad76f24637146bf0a5cc7b32aeb
21BQ1A4213 - BHARGAVA DURGA PRASAD GAJULA <bhargavagajula2003@gmail.com>
2024-01-18 10:53:51 +0530

    full stack lab

---
+++ DRAG&DROP.js
@@ -0,0 +1,15 @@
+
+function allowDrop(ev)
+{
+    ev.preventDefault();
+}
+function drag(ev)
+{
+    ev.dataTransfer.setData("Text/html",ev.target.id);
+}
+function drop(ev)
+{
+    ev.preventDefault();
+    var data=ev.dataTransfer.getData("Text/html");
+    ev.target.appendChild(document.getElementById(data));
+}
